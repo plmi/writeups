1dc7f52b95c17e73119396d19e9856cd71be929f
plmi <65455617+plmi@users.noreply.github.com>
2023-02-10 22:46:01 +0100

    Update challenge01

--- cryptopals/challenge01.py
+++ cryptopals/challenge01.py
@@ -12,8 +12,9 @@ def hex_to_base64(hex_string: str) -> bytes:
 
 def main():
   hex_string: str = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
+  expected: str = 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
   base64_value: str = hex_to_base64(hex_string)
-  print(base64_value.decode('utf-8'))
+  assert expected == base64_value.decode('utf-8'), 'result doesn\'t match expected value'
 
 if __name__ == "__main__":
   main()
